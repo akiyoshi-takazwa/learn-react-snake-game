856ffb7631f83953c5a4c63930c17df69ad6944b
akiyoshi-takazawa <69133113+akiyoshi-takazwa@users.noreply.github.com>
2021-05-14 21:28:41 +0900

    move snake

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 
 import Navigation from './components/Navigation';
 import Field from './components/Field';
@@ -11,6 +11,41 @@ const initialPosition = {x:17, y:17}
 const initialValues = initFields(35, initialPosition)
 const defaultInterval = 100
 
+const GameStatus = Object.freeze({
+  init: 'init',
+  playing: 'playing',
+  suspended: 'suspended',
+  gameover: 'gameover'
+})
+
+export const Direction = Object.freeze({
+  up:'up',
+  right:'right',
+  left:'left',
+  down:'down'
+})
+
+const DirectionKeyCodeMap = Object.freeze({
+  37: Direction.left,
+  38: Direction.up,
+  39: Direction.right,
+  40: Direction.down,
+})
+
+const OppositeDirection = Object.freeze({
+  up: 'down',
+  right: 'left',
+  left: 'right',
+  down: 'up'
+})
+
+const Delta = Object.freeze({
+  up: {x:0, y:-1},
+  right: {x:1, y:0},
+  left:{x:-1, y:0},
+  down: {x:0, y:1},
+})
+
 let timer = undefined
 
 const unsubscribe = () => {
@@ -20,10 +55,22 @@ const unsubscribe = () => {
   clearInterval(timer)
 }
 
+const isCollision = (fieldSize, position) => {
+  if(position.y < 0 || position.x < 0){
+    return true;
+  }
+  if(position.y > fieldSize -1 || position.x > fieldSize -1){
+    return true;
+  }
+  return false;
+}
+
 function App() {
 
   const [fields, setFields] = useState(initialValues)
   const [position, setPosition] = useState()
+  const [status, setStatus] = useState(GameStatus.init)
+  const [direction, setDirection] = useState(Direction.up)
   const [tick, setTick] = useState(0)
 
   useEffect(()=>{
@@ -35,19 +82,66 @@ function App() {
   }, [])
 
   useEffect(()=>{
-    if(!position){
+    if(!position || status !== GameStatus.playing){
       return
     }
-    goUp()
+    const canContinue = handleMoving()
+    if(!canContinue){
+      setStatus(GameStatus.gameover)
+    }
   }, [tick])
 
-  const goUp = () => {
+  const onStart = () => setStatus(GameStatus.playing)
+
+  const onRestart = () => {
+    timer = setInterval(() => {
+      setTick(tick => tick + 1)
+    }, defaultInterval)
+    setDirection(Direction.up)
+    setStatus(GameStatus.init)
+    setPosition(initialPosition)
+    setDirection(Direction.up)
+    setFields(initFields(35, initialPosition))
+  }
+
+  const onChangeDirection = useCallback((newDirection) => {
+    if(status !== GameStatus.playing){
+      return direction
+    }
+    if(OppositeDirection[direction] === newDirection){
+      return
+    }
+    setDirection(newDirection)
+  }, [direction ,status])
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      const newDirection = DirectionKeyCodeMap[e.keyCode];
+      if (!newDirection) {
+        return;
+      }
+
+      onChangeDirection(newDirection);
+    };
+    document.addEventListener('keydown', handleKeyDown);
+    return () => document.removeEventListener('keydown', handleKeyDown)
+  }, [onChangeDirection])
+
+  const handleMoving = () => {
     const {x, y} = position
-    const nextY = Math.max(y-1, 0)
+    const delta = Delta[direction]
+    const newPosition = {
+      x: x + delta.x,
+      y: y + delta.y,
+    }
+    if (isCollision(fields.length, newPosition)) {
+      return false
+    }
     fields[y][x] = ''
-    fields[nextY][x] = 'snake'
-    setPosition({x,y: nextY})
+    fields[newPosition.y][newPosition.x] = 'snake'
+    setPosition(newPosition)
     setFields(fields)
+    return true
   }
 
   return (
@@ -62,8 +156,8 @@ function App() {
         <Field fields={fields} />
       </main>
       <footer className="footer">
-        <Button />
-        <ManipulationPanel />
+        <Button status={status} onStart={onStart} onRestart={onRestart}/>
+        <ManipulationPanel onChange={onChangeDirection}/>
       </footer>
     </div>
   );
